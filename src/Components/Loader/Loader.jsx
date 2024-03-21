2d92f36ded25463e893f5ae486a13236f37d8232
akashkottil <kottilakash@gmail.com>
2024-03-21 19:49:09 +0530

    padding issue fix

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Loader.css'; // Import the CSS file
+
+const Loader = () => {
+    
+  return (
+    <div className="newtons-cradle ">
+      <div className="newtons-cradle__dot"></div>
+      <div className="newtons-cradle__dot"></div>
+      <div className="newtons-cradle__dot"></div>
+      <div className="newtons-cradle__dot"></div>
+    </div>
+  );
+};
+
+export default Loader;
